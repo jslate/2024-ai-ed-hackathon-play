be28082dfb2678ac56a31b855a423254f4fb9e4b
Jonathan Slate <jonathan.slate@pmglearning.com>
2024-07-26 12:11:15 -0400

    Loop and ask more questions

--- run.rb
+++ run.rb
@@ -2,14 +2,30 @@
 
 require './anthropic_chat'
 
+def output(string)
+  blue_text = "\e[34m#{string}\e[0m"
+  puts
+  puts blue_text
+  puts
+end
+
 debug = ARGV[0].nil? ? false : ARGV[0].downcase == 'debug'
 
 chat = AnthropicChat.new(debug:)
 
 name = chat.ask(template_key: :hello, name: 'Jon', schema: { name: 'string' })['name']
-puts "#{name} says hello!"
+output "#{name} says hello!"
 
 response = chat.ask(message: "What's your favorite color, #{name}? Just pretend you do have one!")
+output response
 
-# puts "Your favorite color is #{response}"
-puts response
+loop do
+  print "Prompt: (type 'exit' to quit) "
+  user_input = gets.chomp
+  break if user_input.downcase == 'exit'
+
+  response = chat.ask(message: user_input)
+  output response
+end
+
+puts 'Goodbye!'
